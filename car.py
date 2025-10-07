4fad23bc04e3bf0bcd3e0d4cac9782141f3d9dce
cadee-exe <cadeez@nycstudents.net>
2025-10-07 05:49:26 -0700

    Update car.py

--- car.py
+++ car.py
@@ -1,9 +1,10 @@
 c = ("occupied space")
-"." = ("free space")
+"." == ("free space")
 n = 12
 c = 4
-"." = 4
-if n : c+"."
-print("All spaces were occupied for both days")
-elif:
+"." == 4
+if n == "." + c:
+    print("All spaces were occupied for both days")
+""" elif: """
+print("Not all spaces were occupied for both days")
     
