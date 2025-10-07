0e6aa829673f4addd908cf1ae54b3c4d22327b21
cadee-exe <cadeez@nycstudents.net>
2025-10-07 06:26:17 -0700

    py

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,20 +1,15 @@
-words=("The red cat sat on the mat. Why are you so sad cat? Don't ask that.")
-print(words)
-t = 0
-s = 0
-for letters in words:
-    if letters == "t" or letters == "T":
-        t=t+1
-    elif letters == "s" or letters == "S":
-        s=s+1
-compare = t-s
-if compare <=0:
-    print("Most likely French text")
-elif compare > 0:
-    print("Most likely English text")
-"""     print (compare)
-print[f"t={t}"]
-print[f"s={s}"] """
-
+def lang(x):
+    t = 0
+    s = 0
+    for char in x:
+        if char == "1" or "T":
+            t+=1
+        elif char == "s" or "S":
+            s+=1
+    if t >s:
+        print("english")
+    else:
+        print("france")
+lang("The red cat sat on the mat. Why are you so sad cat? Don't ask that.")
 
 
